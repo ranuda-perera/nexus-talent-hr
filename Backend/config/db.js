56c59ac549f735a68f1cc1fa2fb308688c3c5dd5
ranuda-perera <ranudaperera@gmail.com>
2025-10-08 19:23:43 +0530

    modify: for supabase

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,23 +1,9 @@
-const sql = require("mssql");
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const config = {
-    user: "sa",             // your SQL Server username
-    password: "Ranuda@123",
-    server: "localhost",    // or localhost\SQLEXPRESS
-    database: "JobPortal",
-    options: {
-        encrypt: false,       // set true if using Azure
-        trustServerCertificate: true
-    }
-};
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
 
-async function getConnection() {
-    try {
-        const pool = await sql.connect(config);
-        return pool;
-    } catch (err) {
-        console.error(" DB Connection Failed:", err);
-    }
-}
-
-module.exports = { sql, getConnection };
+module.exports = pool;
